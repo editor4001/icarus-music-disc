7ee152b58ab5a32e3cafe8b6866c88169e4429ae
leodo <30leodomingue@gmail.com>
2026-03-16 12:08:45 +0100

    adding tooltip translation

--- src/main/java/com/example/icarus_music_disc/item/custom/StrawberryItem.java
+++ src/main/java/com/example/icarus_music_disc/item/custom/StrawberryItem.java
@@ -7,7 +7,11 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class StrawberryItem extends Item {
 
@@ -36,5 +40,13 @@ public class StrawberryItem extends Item {
             }
         }
         return super.finishUsingItem(stack, world, entity);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> pComponent, TooltipFlag p_41424_) {
+
+        pComponent.add(Component.translatable("tooltip.icarus_music_disc.strawberry"));
+
+        super.appendHoverText(p_41421_, p_41422_, pComponent, p_41424_);
     }
 }
