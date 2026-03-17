b900a2929779f31d3eac0a1fb5dc5ddcbd779163
leodo <30leodomingue@gmail.com>
2026-03-17 19:07:10 +0100

    added 3d sapphire staff

--- src/main/java/com/example/icarus_music_disc/item/ModItems.java
+++ src/main/java/com/example/icarus_music_disc/item/ModItems.java
@@ -27,6 +27,9 @@ public class ModItems {
     public static final RegistryObject<Item> BURN_ORB = ITEMS.register("burn_orb",
             () -> new FuelItem(new Item.Properties(), 400));
 
+    public static final RegistryObject<Item> SAPPHIRE_STAFF = ITEMS.register("sapphire_staff",
+            () -> new Item(new Item.Properties().stacksTo(1).fireResistant()));
+
 
     public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
             () -> new MetalDetectorItem(new Item.Properties().durability(100)));
